7a0b69abc92ed1042c7101426ef02ad7da5fee69
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2024-12-07 08:14:41 +0100

    chore: update build config after upgrade to Vite 6

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         fileURLToPath(new URL("./src/lib/text.ts", import.meta.url)),
       ],
       formats: ["es"],
+      cssFileName: "style",
     },
     rollupOptions: {
       external: ["vue"],
